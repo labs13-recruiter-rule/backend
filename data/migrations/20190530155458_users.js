6be4881e4d0e4d32bd33b124e290df5b0a930208
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-30 16:03:32 -0500

    added users table

---
+++ data/migrations/20190530155458_users.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("users", table => {
+    table.increments(); // id
+    table.string("first_name").notNullable(); // faker.js firstName
+    table.string("last_name").notNullable(); // faker.js lastName
+    table.string("profile_photo"); // faker.js avatar
+    table.string("email"); // faker.js email
+    table.integer("phone_number"); // faker.js phoneNumber
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("users");
+};
---
+++ knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      filename: "./data/recruiterbe.db3"
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
