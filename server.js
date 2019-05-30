8e59ade5b7c71ef9c31a66adfcb91eefb1de4ac7
omar okla <omaro@me.com>
2019-05-30 16:21:33 -0400

    modify test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = express();
 server.use(express.json());
 
 server.get("/", (req, res) => {
-	res.send(`<p>test, we good </p>`);
+	res.send(`<p>test, we're up and running! </p>`);
 });
 
 module.exports = server;
