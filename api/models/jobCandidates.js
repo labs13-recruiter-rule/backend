25f27d6982ef28676e6b91349ea016a4e9c66486
Duc Vu <de.yd315@gmail.com>
2019-06-04 06:55:45 -0500

    create job candidate model

---
+++ api/models/jobCandidates.js
@@ -0,0 +1,39 @@
+const db = require('../../data/dbConfig');
+
+const table = 'jobCandidates'
+
+const find = () => db(table)
+
+const findById = (id) => db(table).where({ id }).first();
+
+// passing 'id' as the second parameter is recommended to ensure the id is returned
+// when connecting to other database management systems like Postgres
+    
+const add = item => {
+    return db(table)
+    .insert(item, 'id')
+    .then(([id]) => findById(id))
+}
+
+const update = (id, changes) => {
+    return db(table)
+    .where({ id })
+    .update(changes)
+    .then(count => {
+        if (count > 0) {
+            return findById(id)
+        } else {
+            return null
+        } 
+    })
+}
+
+const remove = (id) => db('tracks').where({ id }).del()
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove
+};
