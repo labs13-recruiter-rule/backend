a5285047734f85f1a081a977119cf5721c470048
omar okla <omaro@me.com>
2019-06-05 19:22:42 -0400

    contacts adding works. model and endpoint. returns contact in body post add

--- api/models/userContacts-model.js
+++ api/models/userContacts-model.js
@@ -1,9 +1,29 @@
 const db = require('../../data/dbConfig');
 
 function getContactsByUser(id) {
-  return db('userContacts').where({ id });
+  return db('userContacts').where({ userContactBelongsTo: id });
+}
+
+function getContactByContactID(id) {
+  return db('userContacts')
+    .where({ id })
+    .first();
+}
+
+async function addContactToUserContacts(id, contact) {
+  contact = {
+    ...contact,
+    userContactBelongsTo: id,
+  };
+
+  const [newContactID] = await db('userContacts').insert(contact, 'id');
+  const newContact = await getContactByContactID(newContactID);
+
+  return newContact;
 }
 
 module.exports = {
   getContactsByUser,
+  getContactByContactID,
+  addContactToUserContacts,
 };
--- api/routes/userContacts-router.js
+++ api/routes/userContacts-router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userContacts = require('../models/userContacts-model');
 
+// mergeParams allows us to grab URL params from other/previously defined routes
 const router = express.Router({ mergeParams: true });
 
+// endpoint /users/:id/contacts returns all contacts belonging to that user
 router.get('/', async (req, res) => {
   const id = req.params.userid;
   console.log(id);
@@ -17,7 +19,31 @@ router.get('/', async (req, res) => {
           'No contacts found. Please add some contacts before checking again!',
       });
     }
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const id = req.params.userid;
+  const { name, email } = req.body;
+
+  if (name && email) {
+    try {
+      const newContact = await userContacts.addContactToUserContacts(
+        id,
+        req.body,
+      );
+
+      res.status(201).json(newContact);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  } else {
+    res.status(500).json({
+      message: 'You must provide both a name and an email for a contact.',
+    });
+  }
 });
 
 module.exports = router;
