a1857e3bf4a267e5e739776c6f3cce2c4074eb00
omar okla <omaro@me.com>
2019-06-06 11:52:02 -0400

    migration nullable changes

--- data/migrations/20190530160750_users.js
+++ data/migrations/20190530160750_users.js
@@ -2,12 +2,12 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
     table.increments(); // id
     table.string('firebase_uuid').unique(); // firebase uuid
-    table.string('first_name').notNullable(); // faker.js firstName
-    table.string('last_name').notNullable(); // faker.js lastName
+    table.string('first_name'); // faker.js firstName
+    table.string('last_name'); // faker.js lastName
     table.string('profile_photo'); // faker.js avatar
     table.string('email'); // faker.js email
     table.string('phone_number'); // faker.js phoneNumber
-    table.string('display_name')
+    table.string('display_name');
   });
 };
 
