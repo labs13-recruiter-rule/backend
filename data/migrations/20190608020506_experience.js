6973485e4b54d0727fb3ca341cf5e4e0fd6f75db
ducbavu531@gmail.com <ducbavu531@gmail.com>
2019-06-08 05:15:27 +0700

    created migration for experience

--- data/migrations/20190608020506_experience.js
+++ data/migrations/20190608020506_experience.js
@@ -1,8 +1,18 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('experience', tbl => {
+        tbl.increments();
+        tbl.string('title')
+        tbl.string('company')
+        tbl.string('location')
+        tbl.datetime('start_month')
+        tbl.datetime('start_year')
+        tbl.datetime('end_month')
+        tbl.datetime('end_year')
+        tbl.string('description')
+        tbl.boolean('currently_working')
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('experience');
 };
