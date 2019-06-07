692825c93124c0b78c8ce7a4d078f709ddd68d22
ducbavu531@gmail.com <ducbavu531@gmail.com>
2019-06-08 05:26:00 +0700

    created migration for volunteer

--- data/migrations/20190608020513_skills.js
+++ data/migrations/20190608020513_skills.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('skills', tbl => {
+        tbl.increments();
+        tbl.string('skill')
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('skills');
 };
--- data/migrations/20190608020739_volunteers.js
+++ data/migrations/20190608020739_volunteers.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('volunteer', tbl => {
+        tbl.increments();
+        tbl.string('organization')
+        tbl.string('role')
+        tbl.string('cause')
+        tbl.datetime('start_month')
+        tbl.datetime('start_year')
+        tbl.datetime('end_month')
+        tbl.datetime('end_year')
+        tbl.string('description')
+        tbl.boolean('currently_volunteering')
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('volunteer');
 };
