39942bdb1c2d72956c33ed1b9b6e6ba6ff1c4ee2
ducbavu531@gmail.com <ducbavu531@gmail.com>
2019-06-10 05:52:43 +0700

    creating new migrations for rules table

--- data/migrations/20190608020450_education.js
+++ data/migrations/20190608020450_education.js
@@ -1,17 +1,32 @@
 exports.up = function (knex, Promise) {
-    return knex.schema.createTable('education', tbl => {
+    return knex.schema.createTable('ruleEducation', tbl => {
         tbl.increments();
-        tbl.string('school')
-        tbl.string('degree')
-        tbl.string('field_of_study')
-        tbl.datetime('start_year')
-        tbl.datetime('end_year')
-        tbl.float('grade')
-        tbl.string('activities_and_societies')
-        tbl.string('description')
+        // tbl.string('school')
+        // tbl.string('degree')
+        // tbl.string('field_of_study')
+        // tbl.datetime('start_year')
+        // tbl.datetime('end_year')
+        // tbl.float('grade')
+        // tbl.string('activities_and_societies')
+        // tbl.string('description')
+        
+        tbl
+            .integer('engine_id')
+            .references('id')
+            .inTable('engines');
+        tbl
+            .integer('engine_id')
+            .references('id')
+            .inTable('engines');
+
+        id int [pk]
+        engineId int [ref: > engine.id]
+        keywordContained boolean
+        keyword varchar
+        recipient int [ref: > role.id]
     });
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTableIfExists('education');
+    return knex.schema.dropTableIfExists('ruleEducation');
 };
