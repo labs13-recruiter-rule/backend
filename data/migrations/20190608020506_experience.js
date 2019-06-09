068108b50ec4827315694025c3d914f55321b803
ducbavu531@gmail.com <ducbavu531@gmail.com>
2019-06-10 06:03:49 +0700

    updated the migrations for experience rules table

--- data/migrations/20190608020506_experience.js
+++ data/migrations/20190608020506_experience.js
@@ -1,18 +1,32 @@
 exports.up = function (knex, Promise) {
-    return knex.schema.createTable('experience', tbl => {
+    return knex.schema.createTable('rules_experience', tbl => {
         tbl.increments();
-        tbl.string('title')
-        tbl.string('company')
-        tbl.string('location')
-        tbl.datetime('start_month')
-        tbl.datetime('start_year')
-        tbl.datetime('end_month')
-        tbl.datetime('end_year')
-        tbl.string('description')
-        tbl.boolean('currently_working')
+        // tbl.string('title')
+        // tbl.string('company')
+        // tbl.string('location')
+        // tbl.datetime('start_month')
+        // tbl.datetime('start_year')
+        // tbl.datetime('end_month')
+        // tbl.datetime('end_year')
+        // tbl.string('description')
+        // tbl.boolean('currently_working')
+
+                
+        tbl
+        .integer('engine_id')
+        .references('id')
+        .inTable('engines');
+
+        tbl
+        .integer('send_to')
+        .references('id')
+        .inTable('roles');
+
+        tbl.string('keyword')
+        tbl.boolean('keywordContained')
     });
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTableIfExists('experience');
+    return knex.schema.dropTableIfExists('rules_experience');
 };
