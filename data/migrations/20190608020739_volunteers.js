4b54fa686c056e0a57b0443258ee49a773c82ba2
ducbavu531@gmail.com <ducbavu531@gmail.com>
2019-06-10 06:21:09 +0700

    updated the migrations for volunteer rules table

--- data/migrations/20190608020739_volunteers.js
+++ data/migrations/20190608020739_volunteers.js
@@ -1,19 +1,33 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('volunteer', tbl => {
+    return knex.schema.createTable('rules_volunteer', tbl => {
         tbl.increments();
-        tbl.string('organization')
-        tbl.string('role')
-        tbl.string('cause')
-        tbl.datetime('start_month')
-        tbl.datetime('start_year')
-        tbl.datetime('end_month')
-        tbl.datetime('end_year')
-        tbl.string('description')
-        tbl.boolean('currently_volunteering')
+        // tbl.string('organization')
+        // tbl.string('role')
+        // tbl.string('cause')
+        // tbl.datetime('start_month')
+        // tbl.datetime('start_year')
+        // tbl.datetime('end_month')
+        // tbl.datetime('end_year')
+        // tbl.string('description')
+        // tbl.boolean('currently_volunteering')
+
+
+        tbl
+        .integer('engine_id')
+        .references('id')
+        .inTable('engines');
+
+        tbl
+        .integer('send_to')
+        .references('id')
+        .inTable('roles');
+
+        tbl.string('keyword')
+        tbl.boolean('keywordContained')
     });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('volunteer');
+    return knex.schema.dropTableIfExists('rules_volunteer');
 };
