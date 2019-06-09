ac46f870cf05f57a35d0f2a11ad77c0a1d6cb6a9
ducbavu531@gmail.com <ducbavu531@gmail.com>
2019-06-10 06:20:00 +0700

    updated the migrations for skills rules table

--- data/migrations/20190608020513_skills.js
+++ data/migrations/20190608020513_skills.js
@@ -1,11 +1,24 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('skills', tbl => {
+    return knex.schema.createTable('rules_skills', tbl => {
         tbl.increments();
-        tbl.string('skill')
+        // tbl.string('skill')
+
+        tbl
+        .integer('engine_id')
+        .references('id')
+        .inTable('engines');
+
+        tbl
+        .integer('send_to')
+        .references('id')
+        .inTable('roles');
+
+        tbl.string('keyword')
+        tbl.boolean('keywordContained')
     });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('skills');
+    return knex.schema.dropTableIfExists('rules_skills');
 };
