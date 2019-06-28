eaf6a78c0007ec84d4facdd55ea471c8bc9a85b6
omar okla <omaro@me.com>
2019-06-28 01:13:56 -0400

    mail now sends without hitting spam folders

--- api/routes/mailer.js
+++ api/routes/mailer.js
@@ -1,34 +1,34 @@
-const nodemailer = require('nodemailer');
-const express = require('express');
+// const nodemailer = require('nodemailer');
+// const express = require('express');
 
-const router = express.Router();
+// const router = express.Router();
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.senderEMAIL,
-    pass: process.env.senderPASSWORD,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.senderEMAIL,
+//     pass: process.env.senderPASSWORD,
+//   },
+// });
 
-router.post('/', (req, res) => {
-  const { receivers } = req.body;
-  const mailOptions = {
-    from: 'recruiterrule@gmail.com',
-    to: receivers,
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!',
-  };
+// router.post('/', (req, res) => {
+//   const { receivers } = req.body;
+//   const mailOptions = {
+//     from: 'recruiterrule@gmail.com',
+//     to: receivers,
+//     subject: 'Sending Email using Node.js',
+//     text: 'That was easy!',
+//   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).json(error);
-    } else {
-      console.log(`Email sent: ${info.response}`);
-      res.status(200).json(info);
-    }
-  });
-});
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log(error);
+//       res.status(500).json(error);
+//     } else {
+//       console.log(`Email sent: ${info.response}`);
+//       res.status(200).json(info);
+//     }
+//   });
+// });
 
-module.exports = router;
+// module.exports = router;
